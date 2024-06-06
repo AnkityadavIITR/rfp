e7c3386fb2e8a805f84be1a1f15329fdd5bcd5c1
AnkitIITR <ankit_y@ar.iitr.ac.in>
2024-06-06 15:08:14 +0530

    login

--- src/components/landing-page/auth.tsx
+++ src/components/landing-page/auth.tsx
@@ -1,24 +1,10 @@
 import React from 'react'
-import { SignInButton,SignedOut,SignedIn,UserButton } from '@clerk/nextjs'
-import { Button } from '../ui/button'
+import {UserButton } from '@clerk/nextjs'
 
 const Auth = () => {
   return (
     <div className="absolute right-5 top-5">
-    <SignedOut>
-      <SignInButton mode="modal">
-        <Button
-          id="auth"
-          size="sm"
-          className="h-[32px] rounded text-sm font-medium"
-        >
-          Sign in
-        </Button>
-      </SignInButton>
-    </SignedOut>
-    <SignedIn>
       <UserButton />
-    </SignedIn>
   </div>
   )
 }
